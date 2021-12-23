01d7cdaa5c70b4409481dbd7c2c8362fb2139422
Suyash <shrivastavasuyash15@icloud.com>
2021-12-23 16:53:59 +0530

    final commit

---
+++ src/components/Expenses/ExpenseItem.js
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import ExpenseDate from './ExpenseDate'
+import './ExpenseItem.css'
+import Card from '../UI/Card';
+
+function ExpenseItem (props)
+{    
+    const [title, setTitle] = useState(props.title); 
+   
+   
+    return (
+        <li>
+        <Card className='expense-item'>
+           <ExpenseDate date ={props.date}></ExpenseDate>
+           <div className='expense-item__description'>
+              <h2>{title}</h2>
+              <div className='expense-item__price'>${props.amount}</div>
+           </div>
+        </Card>
+        </li>
+    );
+}
+
+export default ExpenseItem;
